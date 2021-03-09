fe15d847d5f412e2296827167141be91306c2445
chenzl <101du@163.com>
2021-03-09 13:27:30 +0800

    xdai

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -80,5 +80,5 @@ export const WETH = {
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.XDAI]: new Token(ChainId.XDAI, '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d', 18, 'WXDAI', 'Wrapped Ether')
+  [ChainId.XDAI]: new Token(ChainId.XDAI, '0x883Bef7581A94bC69133A9340738260614DaAe70', 18, 'WXDAI', 'Wrapped Ether')
 }
